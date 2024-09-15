90652466f0b3cd1c3a5335add337da67c5f3c711
Elsayed Ahmed <elsayedahmed.official1@gmail.com>
2024-09-15 19:59:18 +0300

    CreationOfDev

--- Company.Web/Program.cs
+++ Company.Web/Program.cs
@@ -28,7 +28,7 @@ namespace Company.Web
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Index Dev}/{id?}");
 
             app.Run();
         }
